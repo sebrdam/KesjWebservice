22dc3f98260665eb9753105cd3f75af24b6a4099
SimplyDread <edwin@simplydread.com>
2014-12-08 13:32:21 +0100

    Test

--- __init__.py
+++ __init__.py
@@ -1,5 +1,9 @@
-from app import app
-app.run(debug=True)
+from flask import Flask
+app = Flask(__name__)
 
+
+@app.route("/")
+def hello():
+    return "Lorem ipsum"
 if __name__ == "__main__":
     app.run()
